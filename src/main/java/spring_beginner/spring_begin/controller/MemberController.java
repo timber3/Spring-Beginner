54df65d2d6061b0c3978f197e3513be13509c952
timber3 <timber3@naver.com>
2023-04-02 23:44:47 +0900

    04.02 MemberController + html form

--- src/main/java/spring_beginner/spring_begin/controller/MemberController.java
+++ src/main/java/spring_beginner/spring_begin/controller/MemberController.java
@@ -2,8 +2,13 @@ package spring_beginner.spring_begin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring_beginner.spring_begin.domain.Member;
 import spring_beginner.spring_begin.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,5 +18,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
---
+++ src/main/java/spring_beginner/spring_begin/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package spring_beginner.spring_begin.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
